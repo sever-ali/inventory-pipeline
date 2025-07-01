8cbfd08b919c44537ce299ab8555b2400a562c07
Sever Ali <s.elruwanduzi@gmail.com>
2025-07-01 16:10:21 +0100

    Added monitoring for clarity

--- inventory_tracker/run.py
+++ inventory_tracker/run.py
@@ -1,7 +1,11 @@
 from app import create_app
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = create_app()
+metrics = PrometheusMetrics(app)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5100, debug=True)
 
